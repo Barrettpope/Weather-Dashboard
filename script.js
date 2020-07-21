4085d0933ba90adcc84a357d606817f961556ac0
Barrett Pope <barrettpope94@gmail.com>
2020-07-21 11:08:44 -0400

    content, styling and logic added to HTML, CSS and JS files.

---
+++ script.js
@@ -0,0 +1,10 @@
+var city = $("#currentCity");
+var date = $("#currentDate");
+var temperature = $("#temperature");
+var humidity = $("#humidity");
+var windSpeed = $("#windSpeed");
+var uvIndex = $("uvIndex");
+
+$.ajax({
+    method: "GET"
+})
