7db8b7ecd9d5d467352a0d5546831c97f229c002
Barrett Pope <barrettpope94@gmail.com>
2020-07-23 23:53:36 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Weather-Dashboard
+Weather-Dashboard is an interactive application that utilizes server-side APIs to display real-time weather data across thousands of cities.
+
+## Description
+Upon entering the applciation, users are greeted with a polished weather dashboard featuring a city search field and weather descriptions with empty data fields. 
+
+Once the user inputs a city name into the search box, weather data is projected next to the corresponding descriptors. The top portion of the weather dashboard displays current weather data, whereas the bottom half of the dashboard features a 5-day forecast. Below is a breakdown of the specific data featured on the site.
+
+Current Weather Tracks:
+    1. Temperature
+    2. Humidity
+    3. Wind Speed
+    4. UV Index
+
+5-Day Forecast Tracks:
+    1. Date
+    2. Temperature
+    3. Humidity
+
+Lastly, users have the ability to enter as many cities as they so choose and click on formerly searched cities to re-display city-specific weather data.
+
+## Visuals
+![](Assets/weather-dashboard.png)
+
+## Link to Deployed Application
+https://barrettpope.github.io/Weather-Dashboard/
+
+## Support
+For questions, comments or concerns regarding the Weather-Dashboard application, please email barrettpope94@gmail.com.
+
+## Contributing
+Pull requests are welcome.
--- script.js
+++ script.js
@@ -1,70 +1,125 @@
 $(document).ready(function () {
 
-    var uvIndex = $("uvIndex");
-    var firstDate = $("#day1");
-    var secondDate = $("#day2");
-    var thirdDate = $("#day3");
-    var fourthDate = $("#day4");
-    var fifthDate = $("#day5");
+    // Empty array of saved cities, derived from user input
+    var savedCities = [];
 
     // Click function on search button displays user input onto the current city div and data from AJAX request onto the corresponding divs
     $("#searchButton").on("click", function () {
         var city = $("#currentCity");
         var userInput = $("#userCity").val();
-        var newSearch = $("<button type=button id=newSearch class=btn-sm>").text(userInput)
-        $(".new-div").append(newSearch);
+
+        if (savedCities.includes(userInput)) {
+            return;
+        } else {
+            savedCities.push(userInput);
+            var newSearch = $("<button type='button' id='newSearch' class='search-btn btn-sm'>").text(userInput)
+            $(".new-div").append(newSearch);
+        }
         city.text(userInput + " | ");
         cityName = userInput;
-        if (userInput === newSearch) {
 
-        }
+        getWeather(userInput);
+        displayDate();
 
-        // Current Weather API
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=e189f3ac24747877cecf335702c81a6e"
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-
-        }).then(function (response) {
-            console.log("This is for general weather ", response);
-            var temp = Math.floor((response.main.temp - 273.15) * 1.80 + 32);
-            console.log(temp)
-            $('#temperature').text(temp + " degrees Farenheit");
-            var humid = response.main.humidity;
-            $("#humidity").text(humid + "%");
-            var ws = response.wind.speed;
-            $("#windSpeed").text(ws + " mph");
-            var lat = response.coord.lat;
-            var lon = response.coord.lon;
-        })
-
-        // 5-Day Forecast API
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=e189f3ac24747877cecf335702c81a6e"
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-
-        }).then(function (response) {
-            for (var i = 0; i < response.list.length; i += 8) {
-                var date1 = response.list[i].dt_txt;
-                var temp1 = Math.floor((response.list[i].main.temp - 273.15) * 1.80 + 32);
-                var humid1 = response.list[i].main.humidity;
-
-                $("#day1").text(date1);
-                $("#tempDay1").text(temp1);
-                $("#humidityDay1").text(humid1);
-            }
-
-            console.log("This is for 5 day forecasts ", response);
-        })
-    })
+    });
 
 });
 
-// TO DO's:
-// Display current date at top
-// Figure out why "&degF" is not working
-// Display UV Index
-// Display 5-day forecast (Possibly 5 for loops needed)
-// Create conditional statement not diplaying duplicate user inputs
-// Copy AJAX code for city button clicks
+//Click event for our city buttons
+$(document).on("click", ".search-btn", function () {
+    var city = $("#currentCity");
+    getWeather($(this).text());
+    var citySearch = $(this).text();
+    city.text(citySearch + " | ");
+
+});
+
+// Function to display date
+function displayDate() {
+    var date = moment().format(("M" + "-" + "D" + "-" + "YYYY") + " |");
+    $("#currentDate").text(date);
+};
+
+// Function that retrieves weather data
+function getWeather(userInput) {
+    // Current Weather API
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=e189f3ac24747877cecf335702c81a6e"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+
+    }).then(function (response) {
+        console.log("This is for general weather ", response);
+        var temp = Math.floor((response.main.temp - 273.15) * 1.80 + 32);
+        console.log(temp)
+        $('#temperature').text(temp + "°F");
+        var humid = response.main.humidity;
+        $("#humidity").text(humid + "%");
+        var ws = response.wind.speed;
+        $("#windSpeed").text(ws + " mph");
+        var icon = response.weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/w/" + icon + ".png";
+        $("#weatherIcon").attr("src", iconURL);
+        lat = response.coord.lat;
+        lng = response.coord.lon;
+        oneCallRequest(lat, lng);
+    });
+
+    // One Call API for UVIndex and Current Date
+    function oneCallRequest(lat, lng) {
+        var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lng + "&appid=e189f3ac24747877cecf335702c81a6e"
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+
+        }).then(function (response) {
+            var uvi = response.current.uvi;
+            $("#uvIndex").text(uvi);
+            if (uvi <= 2) {
+                $("#uvIndex").css({
+                    "background-color": "#88BDBC",
+                    "padding": "10px 10px 10px 10px",
+                    "border": "2px solid #343A3F",
+
+                });
+            } else if (uvi <= 5 && uvi > 2) {
+                $("#uvIndex").css({
+                    "background-color": "#F3D250",
+                    "padding": "10px 10px 10px 10px",
+                    "border": "2px solid #343A3F",
+                });
+            } else {
+                $("#uvIndex").css({
+                    "background-color": "#F78888",
+                    "padding": "10px 10px 10px 10px",
+                    "border": "2px solid #343A3F",
+                });
+            };
+        });
+
+    };
+
+    // 5-Day Forecast API
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=e189f3ac24747877cecf335702c81a6e"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+
+    }).then(function (response) {
+        console.log(response.list)
+
+        for (var i = 0; i < 5; i++) {
+            var c = (i * 8);
+            var dayDiv = $("#day" + (i + 1));
+            var dateDiv = $("#day" + (i + 1)).html(moment().add((i + 1), "days").format("M" + "/" + "D" + "/" + "YYYY"))
+            dayDiv.append(dateDiv);
+            var iconDiv = $("#weatherIcon" + (i + 1));
+            iconDiv.attr("src", "https://openweathermap.org/img/wn/" + response.list[c].weather[0].icon + ".png");
+            var tempDiv = $("#tempDay" + (i + 1))
+            tempDiv.html(Math.floor((response.list[c].main.temp - 273.15) * 1.80 + 32) + "°F");
+            var humidityDiv = $("#humidityDay" + (i + 1));
+            humidityDiv.html(response.list[c].main.humidity + "%")
+
+        };
+    });
+};
